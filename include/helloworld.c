ccb50b61d198dac462e2911ff8fb9611d5d2aca3
i32feraa <i32feraa@uco.es>
2024-03-07 22:23:31 +0100

    esto da fallo

--- include/helloworld.c
+++ include/helloworld.c
@@ -3,8 +3,12 @@ int main(){
     printf("07/03/2024");
     printf("Alberto Fernandez Ramos");
  f();
+ int a = 58;
+
 void f()
 {
+    int b, c;
+    c = a+b;
 char c1[100]= "Hello world";
  char c2[100]= “, I am <your name here>”;
 printf("%s\n", strcat(c1, c2));
